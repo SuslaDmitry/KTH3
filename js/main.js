56d02815f1f84ca2d456136288d6673805a122a3
Dima <yasusianchik@gmail.com>
2021-02-26 01:56:23 +0200

    asd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 ;(function () {
-	
+
+  window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
+
 	'use strict';
 
 	// iPad and iPod detection	
